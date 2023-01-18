e77bdf014d644766794585689fa17ec6d3c6e107
tim.gordeev2015@yandex.ru <tim.gordeev2015@yandex.ru>
2023-01-18 09:05:23 +0300

    Add String and int value in "applicationContext.xml"

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -1,6 +1,27 @@
 public class Person {
     private Pet pet;
 
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("Class Person: set surname");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Class Person: set age");
+        this.age = age;
+    }
+
+    private String surname;
+    private int age;
+
 //    public Person(Pet pet) {
 //        System.out.println("Person bean is created");
 //        this.pet = pet;
