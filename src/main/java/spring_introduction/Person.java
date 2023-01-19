edf664a02feb23c379760fb193906dc48f977eb6
tim.gordeev2015@yandex.ru <tim.gordeev2015@yandex.ru>
2023-01-19 14:29:11 +0300

    @Autowired with constructor injection

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -1,24 +1,25 @@
 package spring_introduction;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("personBean")
 public class Person {
-//    @Autowired
+    //    @Autowired
     private Pet pet;
     private String surname;
     private int age;
 
-    @Autowired
-    public Person(Pet pet) {
+    //    @Autowired
+    public Person(@Qualifier("catBean") Pet pet) {
         System.out.println("Person bean is created");
         this.pet = pet;
     }
 
-    public Person() {
-        System.out.println("Person bean is created");
-    }
+//    public Person() {
+//        System.out.println("Person bean is created");
+//    }
 
     public String getSurname() {
         return surname;
@@ -47,6 +48,6 @@ public class Person {
 
     public void callYourPet() {
         System.out.println("Hello, my lovely Pet!");
-        pet.say();
+            pet.say();
     }
 }
