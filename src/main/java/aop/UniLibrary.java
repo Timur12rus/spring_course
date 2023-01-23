1f98a16e01bbb833c006b0136e2f40a22f8e55d1
tim.gordeev2015@yandex.ru <tim.gordeev2015@yandex.ru>
2023-01-23 16:16:28 +0300

    Pontcat #1 in LogginAspect.java

---
+++ src/main/java/aop/UniLibrary.java
@@ -0,0 +1,21 @@
+package aop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UniLibrary extends AbstractLibrary {
+    @Override
+    public void getBook() {
+        System.out.println("Мы берем книгу из UniLibrary");
+    }
+
+    public String returnBook() {
+        System.out.println("Мы возвращаем книгу в UniLibrary");
+        return "OK";
+    }
+
+    public void getMagazine() {
+        System.out.println("Мы берём журнал из UniLibrary");
+
+    }
+}
