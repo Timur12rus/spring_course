d0287358b5c0b7639ab69065b1e8554209fd487f
tim.gordeev2015@yandex.ru <tim.gordeev2015@yandex.ru>
2023-02-01 12:01:32 +0300

    AOP around Exception

--- src/main/java/aop/Book.java
+++ src/main/java/aop/Book.java
@@ -1,7 +1,27 @@
 package aop;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Book {
+    @Value("Преступление а наказание")
     private String name;
+
+    @Value("Фёдор Михайлович Достоевский")
+    private String author;
+
+    @Value("1866")
+    private int yearOfPublication;
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
 
     public String getName() {
         return name;
--- src/main/java/aop/UniLibrary.java
+++ src/main/java/aop/UniLibrary.java
@@ -7,26 +7,32 @@ public class UniLibrary extends AbstractLibrary {
     @Override
     public void getBook() {
         System.out.println("Мы берем книгу из UniLibrary");
+        System.out.println("--------------------------------------------");
     }
 
-    public void returnBook() {
+    public String returnBook() {
+        int a = 10 / 0;
         System.out.println("Мы возвращаем книгу в UniLibrary");
+        return "Война и мир";
     }
 
     public void getMagazine() {
         System.out.println("Мы берём журнал из UniLibrary");
+        System.out.println("--------------------------------------------");
     }
 
     public void returnMagazine() {
         System.out.println("Мы возвращаем журнал в UniLibrary");
+        System.out.println("--------------------------------------------");
     }
 
-    public void addBook() {
+    public void addBook(String personName, Book book) {
         System.out.println("Мы добавляем книгу в UniLibrary");
+        System.out.println("--------------------------------------------");
     }
 
     public void addMagazine() {
         System.out.println("Мы добавляем журнал в UniLibrary");
+        System.out.println("--------------------------------------------");
     }
-
 }
